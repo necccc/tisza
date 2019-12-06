c0b202b3946265e7024057dff8895b62dcb96610
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2019-12-06 12:38:15 +0100

    Update tito-validate.js

--- middlewares/tito-validate.js
+++ middlewares/tito-validate.js
@@ -4,6 +4,9 @@ const boom = require('boom');
 const { TITO_TOKEN } = process.env
 
 module.exports = (request, response, next) => {
+
+  console.log(request.headers);
+
   const signature = request.headers['tito-signature']
 	const hmac = crypto.createHmac('sha256', TITO_TOKEN);
 	hmac.update(JSON.stringify(request.body));
