2b20dca52f8889f4e1b0814653ad9bc96dd5f989
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2019-12-08 21:07:17 +0100

    payment method fix

--- events.config.js
+++ events.config.js
@@ -8,6 +8,7 @@ module.exports = {
     'reinforce-conf-2020': {
       label: "Reinforce Conference 2020",
       date: '2020. March 12',
+      invoiceIdPrefix: "",
       cateringOf: (ticket) => {
         return 45.4
       },
@@ -15,6 +16,7 @@ module.exports = {
     'jsconf-budapest-2019': {
       date: '2019. September 26-27',
       label: "JSConf Budapest 2019",
+      invoiceIdPrefix: "",
       cateringOf: (ticket) => {
         return 45.4
       },
@@ -22,6 +24,7 @@ module.exports = {
     'jsconf-budapest-2020': {
       date: '2020. September 24-25',
       label: "JSConf Budapest 2020",
+      invoiceIdPrefix: "",
       cateringOf: (ticket) => {
         return 98
       },
@@ -29,6 +32,7 @@ module.exports = {
     'integration-test-event-2020': {
       date: '2020. September 24-25',
       label: "JSConf Budapest 2020",
+      invoiceIdPrefix: "",
       cateringOf: (ticket) => {
         return 45.4
       },
