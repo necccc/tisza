24bd08844a1362900509c40cf99c343d9fe7d47a
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2019-12-09 11:50:59 +0100

    add seller email data

--- events.config.js
+++ events.config.js
@@ -1,22 +1,13 @@
 module.exports = {
-    'reinforce-conf-2019': {
-      label: "Reinforce Conference 2019",
-      cateringOf: (ticket) => {
-        return 45.4
-      },
-    },
     'reinforce-conf-2020': {
       label: "Reinforce Conference 2020",
       date: '2020. March 12',
       invoiceIdPrefix: "",
-      cateringOf: (ticket) => {
-        return 45.4
+      email: {
+        replyToAddress: 'team@jsconfbp.com',
+        subject: 'Your invoice for Reinforce Conference 2020',
+        message: 'Please find your invoice for your Reinforce Conference 2020 ticket purchase.'
       },
-    },
-    'jsconf-budapest-2019': {
-      date: '2019. September 26-27',
-      label: "JSConf Budapest 2019",
-      invoiceIdPrefix: "",
       cateringOf: (ticket) => {
         return 45.4
       },
@@ -25,6 +16,11 @@ module.exports = {
       date: '2020. September 24-25',
       label: "JSConf Budapest 2020",
       invoiceIdPrefix: "",
+      email: {
+        replyToAddress: 'team@jsconfbp.com',
+        subject: 'Your invoice for JSConf Budapest 2020',
+        message: 'Please find your invoice for your JSConf Budapest 2020 ticket purchase.'
+      },
       cateringOf: (ticket) => {
         return 98
       },
@@ -33,6 +29,11 @@ module.exports = {
       date: '2020. September 24-25',
       label: "JSConf Budapest 2020",
       invoiceIdPrefix: "",
+      email: {
+        replyToAddress: 'nec@jsconfbp.com',
+        subject: 'Your invoice for Integration Test Event 2020',
+        message: 'Please find your invoice for your Integration Test Event 2020 ticket purchase.'
+      },
       cateringOf: (ticket) => {
         return 45.4
       },
